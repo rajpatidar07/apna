85214deb1b7079576e84e7ae3b888b40c50f67d4
g-choudhary <g.choudhary.we2code@gmail.com>
2022-10-17 14:43:12 +0530

    sidebar

--- src/components/admin/sidebar.js
+++ src/components/admin/sidebar.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../images/logo.png";
 import Accordion from "react-bootstrap/Accordion";
-import { FaBeer } from 'react-icons/fa';
-import { MdOutlineDashboard} from 'react-icons/md';
-import {BiShoppingBag,BiTransfer,BiCategory} from 'react-icons/bi';
-import {BsBox,BsShopWindow} from 'react-icons/bs';
-import {TbFileInvoice} from 'react-icons/tb';
-import {RiCustomerService2Line,RiAdminLine} from 'react-icons/ri';
+import { FaBeer } from "react-icons/fa";
+import { MdOutlineDashboard } from "react-icons/md";
+import { BiShoppingBag, BiTransfer, BiCategory } from "react-icons/bi";
+import { BsBox, BsShopWindow } from "react-icons/bs";
+import { TbFileInvoice } from "react-icons/tb";
+import { RiCustomerService2Line, RiAdminLine } from "react-icons/ri";
+import { AiOutlineSetting } from "react-icons/ai";
 // import {GrUserAdmin} from 'react-icons/gr';
-
 
 // import Dashboard from './dashboard';
 // import Product from './product';
@@ -27,13 +27,18 @@ function AdminSidebar() {
         <Accordion.Item eventKey="0">
           <Accordion>
             <Link to="/dashboard" className="nav-link nav_heading active">
-            <MdOutlineDashboard /> Dashboard
+              <MdOutlineDashboard /> Dashboard
             </Link>
           </Accordion>
         </Accordion.Item>
 
         <Accordion.Item eventKey="1">
-          <Accordion.Header><Link className="nav-link nav_heading"><BiShoppingBag/>Product</Link></Accordion.Header>
+          <Accordion.Header>
+            <Link className="nav-link nav_heading">
+              <BiShoppingBag />
+              Product
+            </Link>
+          </Accordion.Header>
           <Accordion.Body>
             <ul class="sidebar_navigation_bar">
               <li>
@@ -85,21 +90,21 @@ function AdminSidebar() {
           <Accordion>
             {" "}
             <Link to="/orders" className="nav-link nav_heading">
-            <BsBox /> orders
+              <BsBox /> orders
             </Link>
           </Accordion>
         </Accordion.Item>
         <Accordion.Item eventKey="4">
           <Accordion>
             <Link to="/transactions" className="nav-link nav_heading">
-            <BiTransfer/>   Transactions
+              <BiTransfer /> Transactions
             </Link>
           </Accordion>
         </Accordion.Item>
         <Accordion.Item eventKey="5">
           <Accordion>
             <Link to="/vendors" className="nav-link nav_heading">
-            <BsShopWindow/>  Vendor List
+              <BsShopWindow /> Vendor List
             </Link>
           </Accordion>
         </Accordion.Item>
@@ -107,14 +112,14 @@ function AdminSidebar() {
           <Accordion>
             {" "}
             <Link to="/category" className="nav-link nav_heading">
-            <BiCategory />   Category List
+              <BiCategory /> Category List
             </Link>
           </Accordion>
         </Accordion.Item>
         <Accordion.Item eventKey="7">
           <Accordion>
             <Link to="/invoice" className="nav-link nav_heading">
-            <TbFileInvoice /> Invoices
+              <TbFileInvoice /> Invoices
             </Link>
           </Accordion>
         </Accordion.Item>
@@ -122,7 +127,7 @@ function AdminSidebar() {
           <Accordion>
             {" "}
             <Link to="/complaint" className="nav-link nav_heading">
-            <RiCustomerService2Line/>   Complaints
+              <RiCustomerService2Line /> Complaints
             </Link>
           </Accordion>
         </Accordion.Item>
@@ -130,16 +135,45 @@ function AdminSidebar() {
           <Accordion>
             {" "}
             <Link to="/components" className="nav-link nav_heading">
-            <FaBeer />   Components
+              <FaBeer /> Components
             </Link>
           </Accordion>
         </Accordion.Item>
         <Accordion.Item eventKey="10">
           <Accordion>
             <Link to="/add_admin" className="nav-link nav_heading">
-            <RiAdminLine/> Add/Update Admin
+              <RiAdminLine /> Add/Update Admin
             </Link>
           </Accordion>
+        </Accordion.Item>
+
+        <Accordion.Item eventKey="11">
+          <Accordion.Header>
+            {" "}
+            <Link to="/add_admin" className="nav-link nav_heading">
+              <AiOutlineSetting />
+              Setting
+            </Link>
+          </Accordion.Header>
+          <Accordion.Body>
+            <ul class="sidebar_navigation_bar">
+              <li>
+                <Link to="/" className="nav-link">
+                  Home Page
+                </Link>
+              </li>
+              <li>
+                <Link to="/" className="nav-link ">
+                  Content Manager
+                </Link>
+              </li>
+              <li>
+                <Link to="/" className="nav-link ">
+                  Banner Manager
+                </Link>
+              </li>
+            </ul>
+          </Accordion.Body>
         </Accordion.Item>
       </Accordion>
     </div>
