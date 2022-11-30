bd1e8f056123136ed4e66d75c81be10fd674cf97
NirantarWe2code <nirantar.we2code@gmail.com>
2022-11-30 15:23:53 +0530

    product_details

--- src/components/admin/invoice/invoices.js
+++ src/components/admin/invoice/invoices.js
@@ -1,4 +1,4 @@
-import React, { useState,useMemo } from "react";
+import React, { useState,useMemo,useEffect } from "react";
 import Input from "../common/input";
 import { BsTrash } from "react-icons/bs";
 import { BiEdit } from "react-icons/bi";
@@ -7,25 +7,67 @@ import MainButton from "../common/button";
 import SweetAlert from "sweetalert-react";
 import "sweetalert/dist/sweetalert.css";
 import FilterComponent from "../common/FilterComponent";
-
+import axios from "axios";
 const InvoiceList = () => {
   const handleAlert = () => setAlert(true);
   const hideAlert = () => setAlert(false);
   const [Alert, setAlert] = useState(false);
+  const[invoice,setInvoice]=useState([]);
+  const [SearchInvo, setSearchInvo] = useState({
+    "search":"",
+    "from_date":"",
+    "to_date":""
+    });
+  useEffect(() => {
+    function getInvoiceList() {
+      try {
+        axios
+          .get("http://192.168.29.108:5000/invoice_list")
+          .then((response) => {
+            let data = response.data;
+            setInvoice(data);
+            console.log("invoiceeeeeeeeeeeeeeeeeeeeee--------" + JSON.stringify(data));
+          });
+      } catch (err) {}
+    }
 
+    getInvoiceList();
+  }, []);
+  const onValueChange=(e)=>{
+    setSearchInvo({ ...SearchInvo, [e.target.name]: e.target.value });
+   
+  }
+  const SearchInvoices=()=>{
+    {
+      axios.post(`http://192.168.29.108:5000/invoice_search`,{
+        "search":`${SearchInvo.search}`,
+        "from_date":`${SearchInvo.from_date}`,
+        "to_date":`${SearchInvo.to_date}`
+        // "category_name":`${SearchCat.category_name}`,
+        // "category_type":`${SearchCat.category_type}`,
+        // "level":`${SearchCat.level}`
 
+    }) .then ((response) => {
+      setInvoice(response.data);
+      setSearchInvo('')
+     
+
+      })
+    }
+   
+  }
   const columns = [
     {
       name: "Id",
       selector: (row) => (
-          row.sku
+          row.id
       ),
       sortable: true,
       width: "75px",
     },
     {
       name: "Invoice Number",
-      selector: (row) => row.Inum,
+      selector: (row) => row.invoice_no,
       sortable: true,
       width: "150px",
       center: true,
@@ -36,7 +78,7 @@ const InvoiceList = () => {
     },
     {
       name: "Invoice Date",
-      selector: (row) => row.Idate,
+      selector: (row) => row.invoice_date,
       sortable: true,
       width: "150px",
       center: true,
@@ -48,7 +90,7 @@ const InvoiceList = () => {
    
     {
       name: "Order Date",
-      selector: (row) => row.odate,
+      selector: (row) => row.order_date,
       sortable: true,
       width: "140px",
       center: true,
@@ -58,7 +100,7 @@ const InvoiceList = () => {
     },
     {
       name: "Stock",
-      selector: (row) => row.stock,
+      selector: (row) => row.payment_mode,
       sortable: true,
       width: "100px",
       center: true,
@@ -69,7 +111,7 @@ const InvoiceList = () => {
     },
     {
       name: "Amount",
-      selector: (row) => row.amount,
+      selector: (row) => row.total_quantity,
       sortable: true,
       width: "100px",
       center: true,
@@ -80,7 +122,7 @@ const InvoiceList = () => {
     },
     {
       name: "CGST",
-      selector: (row) => row.CGST,
+      selector: (row) => row.total_cgst,
       sortable: true,
       width: "90px",
       center: true,
@@ -90,7 +132,7 @@ const InvoiceList = () => {
     },
     {
       name: "SGST",
-      selector: (row) => row.SGST,
+      selector: (row) => row.total_sgst,
       sortable: true,
       width: "90px",
       center: true,
@@ -99,7 +141,7 @@ const InvoiceList = () => {
       },
     }, {
       name: "Taxable Value",
-      selector: (row) => row.tval,
+      selector: (row) => row.taxable_value,
       sortable: true,
       width: "90px",
       center: true,
@@ -109,7 +151,7 @@ const InvoiceList = () => {
     },
     {
       name: "Discount/Coupon",
-      selector: (row) => row.discount,
+      selector: (row) => row.discount_coupon,
       sortable: true,
       width: "120px",
       center: true,
@@ -120,7 +162,7 @@ const InvoiceList = () => {
     },
     {
       name: "Total",
-      selector: (row) => row.Total,
+      selector: (row) => row.total_amount,
       sortable: true,
       width: "100px",
       center: true,
@@ -129,70 +171,36 @@ const InvoiceList = () => {
         paddingLeft: "0px",
       },
     },
-    {
-      name: "Action",
-      width: "100px",
-      style: {
-        paddingRight: "12px",
-        paddingLeft: "0px",
-      },
-      center: true,
-      selector: (row) => (
-        <div className={"actioncolimn"}>
-          <BiEdit className=" p-0 m-0  editiconn text-secondary" />
-          <BsTrash
-            className=" p-0 m-0 editiconn text-danger"
-            onClick={handleAlert}
-          />
-        </div>
-      ),
-    },
+    // {
+    //   name: "Action",
+    //   width: "100px",
+    //   style: {
+    //     paddingRight: "12px",
+    //     paddingLeft: "0px",
+    //   },
+    //   center: true,
+    //   selector: (row) => (
+    //     <div className={"actioncolimn"}>
+    //       <BiEdit className=" p-0 m-0  editiconn text-secondary" />
+    //       <BsTrash
+    //         className=" p-0 m-0 editiconn text-danger"
+    //         onClick={handleAlert}
+    //       />
+    //     </div>
+    //   ),
+    // },
   ];
-
-  const data = [
-    {
-      sku: 1,
-      Inum:32,
-      odate: "2022-01-10",
-     Idate:"2022-01-22",
-      amount: "$14",
-      CGST: "10%",
-            SGST: "10%",      
-            tval: "10%",
-      stock: "15",
-      Total:"$25",
-      discount: "50%",
-    },
-    {
-      sku: 2,
-      Inum:33,
-      odate: "2022-01-11",
-     Idate:"2022-01-22",
-      amount: "$14",
-      CGST: "10%",
-            SGST: "10%",      
-            tval: "10%",
-      stock: "15",
-      Total:"$25",
-      discount: "50%",
-    },
-  ];
-
-
-
-  // filter
-
   const [filterText, setFilterText] = React.useState("");
   const [resetPaginationToggle, setResetPaginationToggle] = React.useState(
     false
   );
 
-  const filteredItems = data.filter(
-    item =>
-      JSON.stringify(item)
-        .toLowerCase()
-        .indexOf(filterText.toLowerCase()) !== -1
-  );
+  // const filteredItems = data.filter(
+  //   item =>
+  //     JSON.stringify(item)
+  //       .toLowerCase()
+  //       .indexOf(filterText.toLowerCase()) !== -1
+  // );
 
  
   const subHeaderComponent = useMemo(() => {
@@ -220,19 +228,20 @@ const InvoiceList = () => {
       <div className="card mt-3 p-3 ">
         <div className="row pb-3">
           <div className="col-md-3 col-sm-6 aos_input">
-            <Input type={"text"} plchldr={"Search by Id"} />
+            <Input type={"text"} plchldr={"Search by Id"} value={SearchInvo.search} name={"search"} onChange={(e) => onValueChange(e)} />
           </div>
           <div className="col-md-3 col-sm-6 aos_input">
-          <Input type={"date"} plchldr={"Search by Invoice Date"} />
+          <Input type={"date"} plchldr={"Search by Invoice Date"} value={SearchInvo.from_date} name={"from_date"} onChange={(e) => onValueChange(e)} />
           </div>
           <div className="col-md-3 col-sm-6 aos_input">
-          <Input type={"date"} plchldr={"Search by Order Date"} />
+          <Input type={"date"} plchldr={"Search by Order Date"} value={SearchInvo.to_date} name={"to_date"} onChange={(e) => onValueChange(e)} />
           </div>
 
           <div className="col-md-3 col-sm-6 aos_input">
             <MainButton
               btntext={"Search"}
               btnclass={"button main_button w-100"}
+              onClick={SearchInvoices}
             />
           </div>
         </div>
@@ -244,7 +253,7 @@ const InvoiceList = () => {
 
         <DataTable
           columns={columns}
-          data={filteredItems}
+          data={invoice}
           pagination
           highlightOnHover
           pointerOnHover
@@ -252,14 +261,14 @@ const InvoiceList = () => {
           subHeader
               subHeaderComponent={subHeaderComponent}
         />
-        <SweetAlert
+        {/* <SweetAlert
           show={Alert}
           title="Product Name"
           text="Are you Sure you want to delete"
           onConfirm={hideAlert}
           showCancelButton={true}
           onCancel={hideAlert}
-        />
+        /> */}
       </div>
     </div>
   );
--- src/components/admin/order/order_detail.js
+++ src/components/admin/order/order_detail.js
@@ -4,15 +4,29 @@ import Profile from "../../../images/user.jpg";
 import { AiOutlineFileText } from "react-icons/ai";
 import { BsTelephoneFill, BsFillEnvelopeFill } from "react-icons/bs";
 import { useState } from "react";
+import Form from 'react-bootstrap/Form';
 import axios from "axios";
 const OrderDetail = () => {
   const[order,setOrder]=useState([]);
+  const[user,setUser]=useState([]);
   let orderid = localStorage.getItem("orderid")
+  let userid= localStorage.getItem("userid")
   useEffect(()=>{
     axios.get(`http://192.168.29.108:5000/order_deteils?id=${orderid}`).then((response) => {
       let data = response.data;
       setOrder(data);
       console.log("______uuuuu_____"+JSON.stringify(response.data))
+      // setapicall(false)
+    }).catch(function (error) {
+      console.log(error);
+    });
+  },[])
+  useEffect(()=>{
+    axios.get(`
+    http://192.168.29.108:5000/user_details?user_id=${userid}`).then((response) => {
+      let data = response.data;
+      setUser(data);
+      console.log("______uuuuudataaaaa_____"+JSON.stringify(response.data))
       // setapicall(false)
     }).catch(function (error) {
       console.log(error);
@@ -31,12 +45,7 @@ const OrderDetail = () => {
                     <span>#</span>Order Id
                   </div>
                   <div>
-                    {/* {order.map((orderdata)=>{
-                      return( */}
-                        {/* <span>{orderdata.ref_no}</span> */}
                         <span>yyyy</span>
-                      {/* )
-                    })} */}
                     
                   </div>
                 </div>
@@ -46,13 +55,16 @@ const OrderDetail = () => {
                 </div>
                 <div className="d-flex flex-column text-center">
                   <div className="order_info_heading">Order Status</div>
-                  {/* {order.map(orderdata=>{
-                    return( */}
-                      <div  className="badge bg-warning">pending</div>
-                    {/* )
-                  
-                  })} */}
-                 
+                  <Form.Select aria-label="Floating label select example">
+                  <option>Select Order Status</option>
+                  <option value="delivered">Delivered</option>
+                <option value="pending">Pending</option>
+                <option value="approved">Approved</option>
+                <option value="packed">Processing</option>
+                <option value="return">Return</option>
+                <option value="cancel">Cancel</option>
+      </Form.Select>
+                      {/* <div  className="badge bg-warning">pending</div> */}
                 </div>
                 <div className="d-flex flex-column text-center">
                   <div className="order_info_heading">Order Date & Time</div>
@@ -80,14 +92,12 @@ const OrderDetail = () => {
                   </div>
                 </div>
 
-                <div className="product_price">{orderdata.price}</div>
+                <div className="product_price">{orderdata.price}₹</div>
                 <div className="product_quantity">{orderdata.quantity}</div>
                 <div className="total_amount">{orderdata.price*orderdata.quantity}</div>
               </div>
-
-    )
-                  
-                  })} 
+              )
+            })} 
               
               {/* <div className="d-flex justify-content-between align-items-center">
                 <div className="product_img d-flex">
@@ -106,7 +116,7 @@ const OrderDetail = () => {
 
             <div className="delivery_charges">
               <h5 className="pb-3">Delivery</h5>
-              {/* <div className="d-flex justify-content-between align-items-center">
+              <div className="d-flex justify-content-between align-items-center">
                 <div className="delivery_img d-flex ">
                   <img src="https://media.istockphoto.com/vectors/express-delivery-symbol-vector-id1175078000?b=1&k=20&m=1175078000&s=612x612&w=0&h=2Y5FLXleVSLyaEfZztp2Mhf2pVV6BbqNYkXYs1KHpik=" alt="apnaorganic"/>
                   <div className="delivery_componay ps-3">
@@ -115,7 +125,7 @@ const OrderDetail = () => {
                   </div>
                 </div>
                 <div className="delivery_payment">$20.00</div>
-              </div> */}
+              </div>
             </div>
             <div className="payment_summary">
               <h5 className="pb-3">Payment Summary</h5>
@@ -153,9 +163,76 @@ const OrderDetail = () => {
           </div>
           <div className="col-lg-4">
             <div className="right_side">
-              <div className="customer_name_address">
-                <div className="customer_info">
+              
+              {user.map((userdata)=>{
+                    return(
+                      <div className="customer_name_address">
+                      <div className="customer_info">
                   <div className="customer">Customer</div>
+                  
+                  <div className="customer_name_img d-flex py-3">
+                    <img src={Profile} alt={'apnaorganic'}/>
+                    <div className="customer_name ps-4 my-auto">
+                      {userdata.first_name} {userdata.last_name}
+                    </div>
+                  </div>
+                  <div className="customer_orders d-flex py-3">
+                    <AiOutlineFileText className="order_icon p-1" />
+                    {order.map((orderdata)=>{
+                    return(
+                    <div className="customer_orders_no ps-4 my-auto">
+                    {orderdata.quantity+orderdata.quantity}
+                    </div>
+                    )
+                  })}
+                  </div>
+                  
+                </div>
+                <div className="contact py-3">
+                  <div className="contact_heading pb-3">
+                    <h5>Contact Info</h5>
+                  </div>
+                  <div className="email py-2">
+                    <BsFillEnvelopeFill />
+                    <span>{userdata.email}</span>
+                  </div>
+                  <div className="number py-2">
+                    <BsTelephoneFill />
+                    <span>{userdata.phone_no}</span>
+                  </div>
+                </div>
+                <div className="ship_Address py-3">
+                  <h5>Ship Address</h5>
+                  <div className="address">
+                    <p> {userdata.first_name} {userdata.last_name}</p>
+                    <p>{userdata.gender}</p>
+                    <p>{userdata.date_of_birth}</p>
+                    <p>{userdata.address}</p>
+                    <p>{userdata.address2}</p>
+                    <p>Indore Madhya Pradesh</p>
+                    <p>Pin:452001</p>
+                    <p>{userdata.phone_no}</p>
+                  </div>
+                </div>
+                <div className="bill_Address py-3">
+                  <h5>Bill Address</h5>
+                  <div className="address">
+                    <p>{userdata.first_name} {userdata.last_name}</p>
+                    <p>{userdata.gender}</p>
+                    <p>{userdata.date_of_birth}</p>
+                    <p>{userdata.address}</p>
+                    <p>{userdata.address2}</p>
+                    <p>Indore Madhya Pradesh</p>
+                    <p>Pin:452001</p>
+                    <p>{userdata.phone_no}</p>
+                  </div>
+                </div>
+                 </div>
+                    )
+                  })}
+                {/* <div className="customer_info">
+                  <div className="customer">Customer</div>
+                  
                   <div className="customer_name_img d-flex py-3">
                     <img src={Profile} alt={'apnaorganic'}/>
                     <div className="customer_name ps-4 my-auto">
@@ -203,8 +280,8 @@ const OrderDetail = () => {
                     <p>Pin:452001</p>
                     <p>+91 9876543210</p>
                   </div>
-                </div>
-              </div>
+                </div> */}
+              {/* </div> */}
             </div>
           </div>
         </div>
