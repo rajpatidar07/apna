559339b75032df4a326432472ee3c57eb39c92d8
shivaniwe2code <rahul.verma.we2code@gmail.com>
2022-12-29 14:48:52 +0530

    ser

--- src/components/admin/report/category.js
+++ src/components/admin/report/category.js
@@ -12,184 +12,343 @@ import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
+import moment from "moment/moment";
+import Select from 'react-select'
+import axios from "axios";
+import { useEffect } from "react";
 
 const CategoryReport = () => {
-  const options = {
-    chart: {
-      type: "line",
-      borderRadius: "5",
-      borderColor: "#335cad",
-    },
-    title: {
-      text: " Figures",
-      style: { color: "green", fontSize: "22px" },
-      align: "left",
-    },
-    series: [
-      {
-        name: "Item Sold",
-        data: [1, 2, 1, 4, 3, 6, 9, 4, 1, 8, 3, 5],
-      },
-      {
-        name: "Net Sales",
-        data: [1, 3, 1, 3, 2, 5, 1, 4, 1, 8, 3, 5],
-      },
-      {
-        name: "Orders",
-        data: [2, 1, 6, 7, 4, 6, 2, 4, 1, 8, 3, 5],
-      },
-    ],
-    xAxis: {
-      categories: [
-        "1",
-        "3",
-        "5",
-        "7",
-        "9",
-        "11",
-        "13",
-        "15",
-        "17",
-        "19",
-        "21",
-        "23",
-      ],
-    },
-    yAxis: {
-      categories: ["0", "200", "400", "600", "800", "1000"],
-    },
-  };
-  const columns = [
   
-    {
-      name: "Category",
-      selector: (row) => row.category,
-      sortable: true,
-      width: "260px",
-    },
-    {
-      name: "Item Sold",
-      selector: (row) => row.isold,
-      sortable: true,
-      center: true,
-    },
-    {
-      name: "Net Revenue",
-      selector: (row) => row.net,
-      sortable: true,
-      
-    },
-
-    {
-      name: "Orders",
-      selector: (row) => row.order,
-      sortable: true,
-      
-    },
-    {
-      name: "Products",
-      selector: (row) => row.product,
-      sortable: true,
-     
-    },
-   
-  ];
-
-  const data = [
-    {
-      id: 1,
-      net: "$250",
-      isold: "25",
-      category: <p className="reviewdesc"> Decoration Decoration </p>,
-      product: "120",
-      order:"12"
-    },
-    {
-      id: 2,
-      net: "$250",
-      category: "Backpacks",
-      order: "120",
-      products: "$14",
-      isold: "25",
-    },
-    {
-      id: 1,
-      net: "$250",
-      category: "Backpacks",
-      order: "120",
-      product: "25",
-      isold: "25",
-
-
-    },
-    {
-      id: 2,
-      net: "$250",
-      category: "Backpacks",
-      order: "120",
-      product: "25",
-      isold: "25",
-
-    },
-    {
-      id: 1,
-      net: "$250",
-      category: "Backpacks",
-      order: "120",
-      product: "25",
-      isold: "25",
-
-    },
-    {
-      id: 2,
-      net: "$250",
-      category: "Backpacks",
-      order: "120",
-      product: "25",
-      isold: "25",
-
-    },
-    {
-      id: 1,
-      net: "$250",
-      category: "Backpacks",
-      order: "120",
-      product: "25",
-      isold: "25",
-
-    },
-    {
-      id: 2,
-      net: "$250",
-      category: "Backpacks",
-      order: "120",
-      product: "25",
-      isold: "25",
-
-    },
-    {
-      id: 1,
-      net: "$250",
-      category: "Backpacks",
-      order: "120",
-      product: "25",
-      isold: "25",
-
-    },
-    {
-      id: 2,
-      net: "$250",
-      category: "Backpacks",
-      order: "120",
-      product: "25",
-      isold: "25",
-
-    },
-  ];
   const [filterchange,setFilterchange] = useState('')
+  const [Categoryreport, setCategoryreport] = useState([])
+  const [apicall,setapicall]=useState(false)
+  const [CategoryError,setCategoryError]=useState("")
+ const [fromDate, setFromDate]=useState(moment().format("YYYY-MM-DD"));
+  const [toDate,setToDate]=useState(moment().format("YYYY-MM-DD"))
+ const[category,setCategory]=useState([])
+  const[categoryId,setCategoryId]=useState("")
+
+
+
+  const fetchData=()=>{
+    axios.post(`${process.env.REACT_APP_BASEURL}/categories_report`, 
+    {
+      from_date:fromDate,
+      to_date:toDate,
+      parent_category:categoryId
+  
+    }).then((response) => {
+     
+       console.log("Category data Report ----"+ JSON.stringify(response.data[0]))
+     
+       console.log('Error-----'+JSON.stringify(response.data))
+
+
+       if(response.data.message=="No_Data"){
+
+        setCategoryError(response.data.message)
+        setCategoryreport()
+
+
+      }
+      else{
+        setCategoryError("")
+        
+        setCategoryreport(response.data[0])
+      
+        setapicall(false)
+      }
+
+    }).catch(function (error) {
+      console.log(error);
+    });
+   }
+
+
+
+   const CategoryData= async()=>{
+    let result=  await axios.get(`${process.env.REACT_APP_BASEURL}/category?category=all`)
+    // console.log(result.data)
+    if(result.data){
+      setCategory(result.data)
+    }
+    
+  }
+  
+
+  useEffect(() => {
+    fetchData()
+  
+    CategoryData();
+   
+ }, [apicall]);
+
+
+
   const TimeChange = (e)=>{
     setFilterchange(e.target.value)
+    let value = e.target.value;
+    console.log("---------------------------------------------"+value);
+    if(value==1){
+      setFromDate(moment().format("YYYY-MM-DD"))
+      console.log("From date"+e.target.value)
+      console.log("today")
+      setToDate(moment().format("YYYY-MM-DD"))
+    }
+
+    if(value==2){
+      setFromDate(moment().subtract(1, 'days').startOf('days').format('YYYY-MM-DD'));
+      console.log("From date"+e.target.value);
+     
+      setToDate( moment().format("YYYY-MM-DD"));
+      console.log("yesterday--"+moment().subtract(1, 'day').startOf('day').format('YYYY-MM-DD'));
+
+    }
+   if(value==3){
+      setFromDate( moment().subtract(1, 'weeks').startOf('weeks').format('YYYY-MM-DD')  );
+    
+      console.log("From date"+e.target.value)
+      
+      setToDate( moment().format("YYYY-MM-DD")  );
+      // console.log("last week"+moment().subtract(1, 'week').startOf('week').format('YYYY-MM-DD'))
+   
+   }
+
+   if(value==4){
+   
+
+    setFromDate(moment().subtract(1, 'months').startOf('months').format('YYYY-MM-DD'));
+    console.log("From last month"+e.target.value)
+    setToDate(  moment().format("YYYY-MM-DD")    );
+    // setToDate("2022-12-14");
+
+    
+ }
+ if(value==5){
+  setFromDate(moment().subtract(6, 'month').startOf('month').format('YYYY-MM-DD') );
+  console.log("From last 6 month"+e.target.value)
+  setToDate( moment().format("YYYY-MM-DD") );
+}
+
+
+fetchData()
           }
+
+
+
+ const submitHandler=()=>{
+       
+ setapicall(true)
+ fetchData()
+         
+            
+           }
+        
+
+
+ 
+           const options3 = [
+            category.map((item)=>(
+              { value: `${item.id}` ,label:`${item.category_name}` }
+            ))
+          ]
+          
+          
+          let  CategoryArray=[];
+          
+          const categoryHandler=(e)=>{
+          
+           CategoryArray=[]
+            e.map((item)=>{
+             
+            CategoryArray.push(item.value)
+            
+            })
+            setCategoryId(CategoryArray)
+           
+           }
+
+
+
+
+           var itemSold= Categoryreport.total_sold_product_count
+           var NetSales=Categoryreport.total_sold_product_amount
+           var order=Categoryreport.order_count
+
+           const options = {
+            chart: {
+              type: "bar",
+              borderRadius: "5",
+              borderColor: "#335cad",
+            },
+            title: {
+              text: " Figures",
+              style: { color: "green", fontSize: "22px" },
+              align: "left",
+            },
+            series: [
+              {
+                name: "Item Sold",
+                data: [itemSold],
+              },
+              {
+                name: "Net Sales",
+                data: [NetSales],
+              },
+              {
+                name: "Orders",
+                data: [order],
+              },
+            ],
+            xAxis: {
+              categories: [
+                "1",
+                "3",
+                "5",
+                "7",
+                "9",
+                "11",
+                "13",
+                "15",
+                "17",
+                "19",
+                "21",
+                "23",
+              ],
+            },
+            yAxis: {
+              categories: ["0", "200", "400", "600", "800", "1000"],
+            },
+          };
+          const columns = [
+          
+            {
+              name: "Category",
+              selector: (row) => row.category,
+              sortable: true,
+              width: "260px",
+            },
+            {
+              name: "Item Sold",
+              selector: (row) => row.isold,
+              sortable: true,
+              center: true,
+            },
+            {
+              name: "Net Revenue",
+              selector: (row) => row.net,
+              sortable: true,
+              
+            },
+        
+            {
+              name: "Orders",
+              selector: (row) => row.order,
+              sortable: true,
+              
+            },
+            {
+              name: "Products",
+              selector: (row) => row.product,
+              sortable: true,
+             
+            },
+           
+          ];
+        
+          const data = [
+            {
+              id: 1,
+              net: "$250",
+              isold: "25",
+              category: <p className="reviewdesc"> Decoration Decoration </p>,
+              product: "120",
+              order:"12"
+            },
+            {
+              id: 2,
+              net: "$250",
+              category: "Backpacks",
+              order: "120",
+              products: "$14",
+              isold: "25",
+            },
+            {
+              id: 1,
+              net: "$250",
+              category: "Backpacks",
+              order: "120",
+              product: "25",
+              isold: "25",
+        
+        
+            },
+            {
+              id: 2,
+              net: "$250",
+              category: "Backpacks",
+              order: "120",
+              product: "25",
+              isold: "25",
+        
+            },
+            {
+              id: 1,
+              net: "$250",
+              category: "Backpacks",
+              order: "120",
+              product: "25",
+              isold: "25",
+        
+            },
+            {
+              id: 2,
+              net: "$250",
+              category: "Backpacks",
+              order: "120",
+              product: "25",
+              isold: "25",
+        
+            },
+            {
+              id: 1,
+              net: "$250",
+              category: "Backpacks",
+              order: "120",
+              product: "25",
+              isold: "25",
+        
+            },
+            {
+              id: 2,
+              net: "$250",
+              category: "Backpacks",
+              order: "120",
+              product: "25",
+              isold: "25",
+        
+            },
+            {
+              id: 1,
+              net: "$250",
+              category: "Backpacks",
+              order: "120",
+              product: "25",
+              isold: "25",
+        
+            },
+            {
+              id: 2,
+              net: "$250",
+              category: "Backpacks",
+              order: "120",
+              product: "25",
+              isold: "25",
+        
+            },
+          ];
+          
+          
   return (
     <div>
       <h2>Category Report</h2>
@@ -203,33 +362,52 @@ const CategoryReport = () => {
               placeholder="Search by category"
               onChange={TimeChange}
             >
-              <option>Search by category</option>
-              <option value="1">1 day</option>
-              <option value="2">1 week</option>
-              <option value="3">current month</option>
-              <option value="4">last month</option>
-              <option value="5">last 6  month</option>
-              <option value="6">custom month</option>
-              <option value="7">custom date</option>
+             <option >Search by category</option>
+              <option name="today" value={1}>Today</option>
+              <option name="yesterday" value={2}>yesterday</option>
+              <option name="last_week" value={3}>Last week</option>
+              <option name="last_month" value={4}>last month</option>
+              <option name="last_6_month" value={5}>last 6  month</option>
+              {/* <option name="custom_month" value="6">custom month</option> */}
+              <option name="custom_date" value="7">custom date</option>
 
             </Form.Select>
-          </div>
+            </div>
+            <div className="col-md-3 col-sm-6 aos_input">
+            <Select
+      
+              className=" basic-multi-select"
+              placeholder="Search by Category"
+              onChange={categoryHandler}
+             
+              classNamePrefix="select"
+              isMulti  
+              options={options3[0]} 
+            />
+         
+            </div>
           {filterchange==='7'?
-          <>
-      <div className="col-md-3 col-sm-6 aos_input">
-        <Input type={"date"} plchldr={"Search by date"} />
-        </div>
-        
-        <div className="col-md-3 col-sm-6 aos_input">
-        <Input type={"date"} plchldr={"Search by date"} />
-        </div>
-        </>
-        :filterchange==='6'? <div className="col-md-3 col-sm-6 aos_input">
-        <Input type={"month"} plchldr={"Search by month"} />
-        </div> : null}
-        <div className="col-md-auto col-sm-6 aos_input">
-        <MainButton btntext={"Search"} btnclass={'button main_button'} />
-        </div>
+                  
+                  <div>
+             
+                  <div className="col-md-3 col-sm-6 aos_input">
+                    <input type={"date"} placeholder={"Search by date"} onChange={(e)=>{setFromDate(e.target.value)}} className={'adminsideinput'}/>
+                    </div>
+                    
+                    <div className="col-md-3 col-sm-6 aos_input">
+                    <input type={"date"} placeholder={"Search by date"} onChange={(e)=>{setToDate(e.target.value)}} className={'adminsideinput'}/>
+                    </div>
+                    </div>
+                    :filterchange==='6'? <div className="col-md-3 col-sm-6 aos_input">
+                    <Input type={"month"} plchldr={"Search by month"} />
+                   
+                    </div> : null}
+                    
+                    
+                    <div className="col-md-auto col-sm-6 aos_input">
+                    <MainButton btntext={"Search"} btnclass={'button main_button'} onClick={submitHandler} />
+                    </div>
+
         <div className="col-md-auto col-sm-6 aos_input">
         <DropdownButton id="dropdown-variant-success" title="Download" variant="button main_button">
       <Dropdown.Item href="#/action-1">Excel</Dropdown.Item>
@@ -256,7 +434,10 @@ const CategoryReport = () => {
                 <div className="col-12">
                   <div className="row  d-flex flex-column align-items-center">
                     <div className="d-flex align-items-baseline justify-content-between">
-                      <h3>2,356</h3>
+                    {console.log("roroororo---"+CategoryError)}
+                    {console.log("dataaaa-"+Categoryreport.total_sold_product_count)}
+                    {(CategoryError)=="No_Data"||(Categoryreport.total_sold_product_count)==null || (Categoryreport.total_sold_product_count)==undefined  || (Categoryreport.total_sold_product_count)==""?<h3>No Record</h3>: <h3>{Categoryreport.total_sold_product_count}</h3>}
+
                       <div className="d-flex align-items-center justify-content-center">
                         <AiOutlineArrowRight className="h5 mb-0 mx-2" />
                         <p className="mb-0 h5">0%</p>
@@ -281,7 +462,7 @@ const CategoryReport = () => {
                 <div className="col-12">
                   <div className="row  d-flex flex-column align-items-center">
                     <div className="d-flex align-items-baseline justify-content-between">
-                      <h3>2,356</h3>
+                    {(CategoryError)=="No_Data"||(Categoryreport.total_sold_product_amount)==null || (Categoryreport.total_sold_product_amount)==undefined  || (Categoryreport.total_sold_product_amount)==""?<h3>No Record</h3>: <h3>{Categoryreport.total_sold_product_amount}</h3>}
                       <div className="d-flex align-items-center justify-content-center">
                         <AiOutlineArrowRight className="h5 mb-0 mx-2" />
                         <p className="mb-0 h5">0%</p>
@@ -306,7 +487,8 @@ const CategoryReport = () => {
                 <div className="col-12">
                   <div className="row  d-flex flex-column align-items-center">
                     <div className="d-flex align-items-baseline justify-content-between">
-                      <h3>2,356</h3>
+                    {(CategoryError)=="No_Data"||(Categoryreport.order_count)==null || (Categoryreport.order_count)==undefined  || (Categoryreport.order_count)==""?<h3>No Record</h3>: <h3>{Categoryreport.order_count}</h3>}
+
                       <div className="d-flex align-items-center justify-content-center">
                         <AiOutlineArrowRight className="h5 mb-0 mx-2" />
                         <p className="mb-0 h5">0%</p>
@@ -327,7 +509,8 @@ const CategoryReport = () => {
         {/*  */}
 
         {/* graph */}
-        <HighchartsReact highcharts={Highcharts} options={options} />
+        {(Categoryreport.total_sold_product_count)||(Categoryreport.total_sold_product_amount)||Categoryreport.order_count?   <HighchartsReact highcharts={Highcharts} options={options} />:null}
+     
 
         {/*  */}
 
