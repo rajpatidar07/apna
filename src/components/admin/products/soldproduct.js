3ec37f7a9eeefebe86698f986800a22adfa444c1
RohitWe2code <rohit.we2code@gmail.com>
2023-02-13 17:31:41 +0530

    Made many changes and resolved bugs in expired products.

--- src/components/admin/products/soldproduct.js
+++ src/components/admin/products/soldproduct.js
@@ -5,17 +5,16 @@ import Form from "react-bootstrap/Form";
 import { BiEdit } from "react-icons/bi";
 import { Modal } from "react-bootstrap";
 import axios from "axios";
+import BrandJson from "./../json/BrandJson";
 import SAlert from "../common/salert";
 
 const Soldproduct = () => {
   const formRef = useRef();
-
   const handleAlert = () => setAlert(true);
   const hideAlert = () => setAlert(false);
   const [Alert, setAlert] = useState(false);
-
   const handleClick = () => {};
-
+  let token = localStorage.getItem("token");
   const [show, setShow] = useState(false);
   const [id, setId] = useState("");
   const [productData, setProductData] = useState({});
@@ -24,9 +23,12 @@ const Soldproduct = () => {
   const [UpdateAlert, setUpdateAlert] = useState(false);
   let [searcherror, setsearcherror] = useState(false);
   const [filtervategory, setfiltercategory] = useState([]);
+  const [vendorid, setVendorId] = useState([]);
   const [searchdata, setsearchData] = useState({
     product_title_name: "",
     category: [],
+    vendor: [],
+    brand: [],
   });
   // console.log("*****************----------" + JSON.stringify(solddata));
   const closeUpdateAlert = () => {
@@ -43,7 +45,33 @@ const Soldproduct = () => {
         });
     } catch (err) {}
   };
+  /*<---vendor list api---> */
+  const getVendorData = () => {
+    try {
+      axios
+        .post(
+          `${process.env.REACT_APP_BASEURL}/vendors`,
+          { vendor_id: "all" },
+          {
+            headers: { admin_token: `${token}` },
+          }
+        )
+        .then((response) => {
+          let cgory = response.data;
 
+          const result = cgory.filter(
+            (thing, index, self) =>
+              index === self.findIndex((t) => t.shop_name == thing.shop_name)
+          );
+          const result1 = result.filter(
+            (item) => item.status === "approved" || item.status === "active"
+          );
+          setVendorId(result1);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
   const handleShow = (id, product_id) => {
     try {
       axios
@@ -76,7 +104,12 @@ const Soldproduct = () => {
   };
 
   const OnReset = () => {
-    setsearchData({ product_title_name: "", category: "" });
+    setsearchData({
+      product_title_name: "",
+      category: "",
+      vendor: "",
+      brand: "",
+    });
     setapicall(true);
     setsearcherror(false);
   };
@@ -85,15 +118,17 @@ const Soldproduct = () => {
     axios
       .post(`${process.env.REACT_APP_BASEURL}/home?page=0&per_page=400`, {
         product_search: {
-          search: "",
+          search: [`${searchdata.product_title_name}`],
           price_from: "",
           price_to: "",
           id: "",
           sale_price: "",
           product_title_name_asc_desc: "",
           short_by_updated_on: "",
-          product_title_name: [`${searchdata.product_title_name}`],
+          // product_title_name: [`${searchdata.product_title_name}`],
           category: [`${searchdata.category}`],
+          brand: [`${searchdata.brand}`],
+          shop: [`${searchdata.vendor}`],
           quantity: ["0"],
           is_delete: ["1"],
         },
@@ -112,35 +147,27 @@ const Soldproduct = () => {
         console.log(error);
       });
     getCategorydatafilter();
+    getVendorData();
   }, [apicall]);
 
   const columns = [
     {
-      name: "Sku",
-      selector: (row) => <p>{row.id}</p>,
-      sortable: true,
-      width: "150px",
-      center: true,
-    },
-    {
       name: "#",
-      width: "180px",
+      width: "100px",
       center: true,
       cell: (row) => (
         <img
-          height="90px"
-          width="70px"
-          alt={row.product_title_name}
+          alt={"apna_organic"}
           src={
-            row.image
-              ? row.image
-              : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
+            row.all_images
+              ? row.all_images
+              : "https://t3.ftcdn.net/jpg/05/37/73/58/360_F_537735846_kufBp10E8L4iV7OLw1Kn3LpeNnOIWbvf.jpg"
           }
           style={{
-            borderRadius: 10,
-            paddingTop: 10,
-            paddingBottom: 10,
+            padding: 10,
             textAlign: "right",
+            maxHeight: "100px",
+            maxWidth: "100px",
           }}
           onClick={handleClick}
         />
@@ -148,27 +175,123 @@ const Soldproduct = () => {
     },
     {
       name: "Product Name",
-      selector: (row) => row.product_title_name,
+      selector: (row) => (
+        <div>
+          <p className="mb-1" onClick={(row.id, row.product_id)}>
+            <b>
+              {row.product_title_name}
+              <br />
+            </b>
+            {/* Product ID: {row.product_id} <br /> */}
+            <div className="d-flex flex-column ">
+              {row.is_featured === 1 ? (
+                <span className={"badge bg-warning mt-1"}>
+                  {"featured product"}
+                </span>
+              ) : null}
+              {row.is_special_offer === 1 ? (
+                <span className={"badge bg-info mt-1"}>{"special offer"}</span>
+              ) : null}
+            </div>
+          </p>
+        </div>
+      ),
       sortable: true,
-      width: "280px",
+      width: "200px",
     },
     {
       name: "Category",
       selector: (row) => row.category,
       sortable: true,
-      width: "200px",
+      width: "90px",
     },
-    // {
-    //   name: "Quantity",
-    //   selector: (row) => row.quantity,
-    //   sortable: true,
-    //   width: "200px",
-    // },
     {
-      name: "Price",
-      selector: (row) => row.product_price,
+      name: "Vendor",
+      selector: (row) => row.shop,
       sortable: true,
-      width: "180px",
+      width: "90px",
+    },
+    {
+      name: "Product Type",
+      selector: (row) => row.product_type,
+      sortable: true,
+      width: "90px",
+    },
+    {
+      name: "Brand",
+      selector: (row) => row.brand,
+      sortable: true,
+      width: "100px",
+    },
+    {
+      name: "Mrp",
+      selector: (row) => (
+        <p className="m-0">
+          <b>MRP :</b>₹ {Number(row.mrp).toFixed(2)} <br />
+          <b>Discount : </b>
+          {Number(row.discount).toFixed(2)}%
+          {/* {row.discount === "0" ? null : row.discount + "%"}{" "} */}
+          <br />
+          <b>Product Price:</b>₹ {Number(row.product_price).toFixed(2)} <br />
+          <b>Sale Price:</b>₹ {Number(row.sale_price).toFixed(2)} <br />
+        </p>
+      ),
+      sortable: true,
+      width: "130px",
+      center: true,
+      style: {
+        paddingRight: "32px",
+        paddingLeft: "0px",
+      },
+    },
+
+    {
+      name: "Tax",
+      selector: (row) => (
+        <div className="d-flex flex-column">
+          <b>
+            Total:
+            {Number(row.gst) +
+              Number(row.cgst) +
+              Number(row.sgst) +
+              Number(row.wholesale_sales_tax) +
+              Number(row.retails_sales_tax) +
+              Number(row.manufacturers_sales_tax) +
+              Number(row.value_added_tax) +
+              "%"}{" "}
+          </b>{" "}
+          <div className="d-flex">
+            <b>Gst :</b>₹ {Number(row.gst).toFixed(2)}%<b>Cgst : </b>
+            {Number(row.cgst).toFixed(2)}%
+            {/* {row.discount === "0" ? null : row.discount + "%"}{" "} */}
+            <b>Sgst:</b> {Number(row.sgst).toFixed(2)}%
+          </div>
+          <div className="d-flex flex-column">
+            <b>
+              wholesale_sales_tax:{Number(row.wholesale_sales_tax).toFixed(2)}%
+            </b>{" "}
+            <b>retails_sales_tax:{Number(row.retails_sales_tax).toFixed(2)}%</b>{" "}
+            <b>value_added_tax:{Number(row.value_added_tax).toFixed(2)}% </b>
+            <b>
+              manufacturers_sales_tax:{" "}
+              {Number(row.manufacturers_sales_tax).toFixed(2)}%
+            </b>{" "}
+          </div>
+        </div>
+      ),
+
+      sortable: true,
+      width: "200px",
+      center: true,
+      style: {
+        paddingLeft: "0px",
+      },
+    },
+    {
+      name: "SP",
+      selector: (row) => row.sale_price.toFixed(2),
+      sortable: true,
+      width: "100px",
       center: true,
       style: {
         paddingRight: "32px",
@@ -251,6 +374,46 @@ const Soldproduct = () => {
                   <option value={data.id} key={i}>
                     {" "}
                     {data.id}
+                  </option>
+                );
+              })}
+            </Form.Select>
+          </div>
+          <div className="col-md-2 col-sm-6 aos_input">
+            <Form.Select
+              aria-label="Search by status"
+              className="adminselectbox"
+              placeholder="Search by vendor"
+              onChange={OnSearchChange}
+              name="vendor"
+              value={String(searchdata.vendor)}
+            >
+              <option value={""}>Select Vendor</option>
+              {(vendorid || []).map((data, i) => {
+                return (
+                  <option value={data.shop_name} key={i}>
+                    {" "}
+                    {data.shop_name}
+                  </option>
+                );
+              })}
+            </Form.Select>
+          </div>
+          <div className="col-md-2 col-sm-6 aos_input">
+            <Form.Select
+              aria-label="Search by brand"
+              className="adminselectbox"
+              placeholder="Search by brand"
+              onChange={OnSearchChange}
+              name="brand"
+              value={String(searchdata.brand)}
+            >
+              <option value={""}>Select Brand</option>
+              {(BrandJson.BrandJson || []).map((data, i) => {
+                return (
+                  <option value={data} key={i}>
+                    {" "}
+                    {data}
                   </option>
                 );
               })}
